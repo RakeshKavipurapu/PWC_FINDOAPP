e7172d8ede156538eb424e0a51d8dd8cf8562af4
Rakesh <Rakesh@host.docker.internal>
2021-07-28 11:13:42 +0530

    FINDO APP V2.0 without SNS

---
+++ SpringBootRegistrationLogin/src/main/java/net/codejava/User.java
@@ -0,0 +1,107 @@
+package net.codejava;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DonarDetails")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+	
+	@Column(nullable = false, length = 64)
+	private String password;
+	
+	@Column(name = "first_name", nullable = false, length = 20)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false, length = 20)
+	private String lastName;
+
+
+	@Column(name = "Address", nullable = false, length = 50)
+	private String address;
+
+
+	@Column(name = "Contact", nullable = false, length = 200)
+	private String contact;
+
+
+	@Column(name = "Anonymous", nullable = false, length = 20)
+	private String anonymous;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getAnonymous() {
+		return anonymous;
+	}
+
+	public void setAnonymous(String anonymous) {
+		this.anonymous = anonymous;
+	}
+
+}
